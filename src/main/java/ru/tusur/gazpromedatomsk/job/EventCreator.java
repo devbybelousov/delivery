ee2403c0ddfda5f050abc67bc03492057057b17c
Dmitry Belousov <devbybelousov@yandex.ru>
2021-05-23 16:11:36 +0700

    Фикс багов

--- src/main/java/ru/tusur/gazpromedatomsk/job/EventCreator.java
+++ src/main/java/ru/tusur/gazpromedatomsk/job/EventCreator.java
@@ -18,7 +18,7 @@ public class EventCreator {
   private final FileService fileService;
 
   @Scheduled(cron = "0 0 6 * * ?", zone = "GMT+7:00")
-  @Scheduled(cron = "0 10 16 * * ?", zone = "GMT+7:00")
+  @Scheduled(cron = "0 16 16 * * ?", zone = "GMT+7:00")
   public void createMenu() {
     menuParse.parse();
     log.debug("Parse Success!");
